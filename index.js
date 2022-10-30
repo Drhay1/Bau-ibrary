1b327b0dc280336b59bee8ba80e54594c09bfdc9
Oluwadamilare Emmanuel Akerele <56739109+Drhay1@users.noreply.github.com>
2022-10-30 13:08:30 +0300

    my bau project
    version 1 without backend

---
+++ index.js
@@ -0,0 +1,12 @@
+// const button = document.querySelector(".btn");
+// const body = document.querySelector("body");
+
+// button.addEventListener("click", btn);
+
+
+
+
+// function btn(){
+//     body.style.backgroundColor = "black";
+//     console.log("button is clicked");
+// }
